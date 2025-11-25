771fb485f7b624bec0f55311882aa45ef36ae4dc
lab177147 <xdok177147@gmail.com>
2025-11-25 08:37:29 +0000

    4_1.1

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // define endpoint for exercise 1 here
-app.get('/math/circle/:r', (req, res) => {
-//TODO1  
-  res.json(result);
+app.get("/math/circle/:r", (req, res) => {
+  const r = Number(req.params.r);
+
+  // check for required parameters
+  if (isNaN(r)) {
+    return res.status(400).send("Parameter 'r' must be a number");
+  }
+  const area = Math.PI * r * r;
+  const circumference = Math.PI * 2 * r;
+
+  const result = {
+    area: area,
+    circumference: circumference,
+  };
+  res.type("text").send(result);
 });
 
 //TODO2
 
-
 //TODO3
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
